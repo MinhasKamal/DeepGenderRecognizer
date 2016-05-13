bd51f9787d4932721f3385e74dbbb029792d8533
MinhasKamal <minhaskamal024@gmail.com>
2016-05-13 21:58:17 +0600

    project initiated

---
+++ src/com/minhaskamal/deepGenderRecognizer/DeepGenderRecognizer.java
@@ -0,0 +1,13 @@
+/***********************************************************
+* Developer: Minhas Kamal (minhaskamal024@gmail.com)       *
+* Website: https://github.com/MinhasKamal/Intellectron     *
+* License:  GNU General Public License version-3           *
+***********************************************************/
+
+package com.minhaskamal.deepGenderRecognizer;
+
+public class DeepGenderRecognizer {
+	public static void main(String[] args) {
+		System.out.println("Hallow DeepGenderRecognizer!");
+	}
+}
